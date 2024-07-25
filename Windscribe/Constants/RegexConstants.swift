2300d2a27be7e7fada7a2a92ef885289df564cb0
Ginder Singh <ginder@windscribe.com>
2024-07-25 17:39:34 -0400

    Updated to v3.8.0

---
+++ Windscribe/Constants/RegexConstants.swift
@@ -0,0 +1,14 @@
+//
+//  RegexConstants.swift
+//  Windscribe
+//
+//  Created by Andre Fonseca on 09/07/2024.
+//  Copyright © 2024 Windscribe. All rights reserved.
+//
+
+import Foundation
+
+struct RegexConstants {
+    static let urlHttpsRegex = #"https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"#
+    static let urlTlsRegex = #"[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)"#
+}
