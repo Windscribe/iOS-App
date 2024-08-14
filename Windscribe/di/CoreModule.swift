a40f46605d4eea5557f3846f3b765830cc27015b
Ginder Singh <ginder@windscribe.com>
2024-08-14 10:40:23 -0400

    use remote upstream resolver for wsnet.

--- Windscribe/di/CoreModule.swift
+++ Windscribe/di/CoreModule.swift
@@ -34,7 +34,7 @@ extension Container {
         #else
             WSNet.initialize("ios", platformName: "ios", appVersion: Bundle.main.releaseVersionNumber ?? "", deviceId: UIDevice.current.identifierForVendor?.uuidString ?? "", openVpnVersion: APIParameterValues.openVPNVersion, isUseStagingDomains: false, persistentSettings: preferences.getServerSettings())
         #endif
-            setWSNetDNSServer(servers: preferences.getCustomDNSValue().servers, ext: ext)
+            WSNet.instance().dnsResolver().setDnsServers(["76.76.2.0", "1.1.1.1", "9.9.9.9"])
             WSNet.instance().setConnectivityState(true)
             WSNet.instance().setIsConnectedToVpnState(false)
             WSNet.instance().advancedParameters().setAPIExtraTLSPadding(preferences.isCircumventCensorshipEnabled())
