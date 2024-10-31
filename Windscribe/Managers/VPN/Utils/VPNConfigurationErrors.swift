f1a3812b308ec9574e70390d94847eca47eeced7
Ginder Singh <ginder@windscribe.com>
2024-10-31 03:26:34 -0400

    drfat

---
+++ Windscribe/Managers/VPN/Utils/VPNConfigurationErrors.swift
@@ -0,0 +1,9 @@
+//
+//  VPNConfigurationErrors.swift
+//  Windscribe
+//
+//  Created by Ginder Singh on 2024-10-30.
+//  Copyright © 2024 Windscribe. All rights reserved.
+//
+
+import Foundation
---
+++ Windscribe/Managers/VPN/Utils/VPNManagerUtils+config.swift
@@ -0,0 +1,9 @@
+//
+//  VPNManagerUtils+config.swift
+//  Windscribe
+//
+//  Created by Ginder Singh on 2024-10-30.
+//  Copyright © 2024 Windscribe. All rights reserved.
+//
+
+import Foundation
