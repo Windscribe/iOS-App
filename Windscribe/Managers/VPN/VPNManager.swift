1a34189c4c533127d2be12e2599514b3f1ae058c
Andre Fonseca <andre@windscribe.com>
2024-11-27 18:13:41 +0000

    need to get connectionstate after updating configurationState

--- Windscribe/Managers/VPN/VPNManager.swift
+++ Windscribe/Managers/VPN/VPNManager.swift
@@ -141,6 +141,7 @@ class VPNManager: VPNManagerProtocol {
             configureStateLock.lock()
             _configurationState = newValue
             configureStateLock.unlock()
+            configureForConnectionState()
         }
     }
 
