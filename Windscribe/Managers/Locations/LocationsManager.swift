25325e18d2113036829f528af215696f31bd1aa1
Andre Fonseca <andre@windscribe.com>
2024-11-28 17:20:02 +0000

    Lets have Locations Manager as a single point of truth for the selected locations

--- Windscribe/Managers/Locations/LocationsManager.swift
+++ Windscribe/Managers/Locations/LocationsManager.swift
@@ -6,16 +6,29 @@
 //  Copyright © 2024 Windscribe. All rights reserved.
 //
 
+import RxSwift
+
 protocol LocationsManagerType {
     func getBestLocationModel(from groupId: String) -> BestLocationModel?
     func getLocation(from groupId: String) throws -> (Server, Group)
+    func saveLastSelectedLocation(with locationID: String)
+    func saveBestLocation(with locationID: String)
+    func selectBestLocation(with locationID: String)
+    func getBestLocation() -> String
+    func getLastSelectedLocation() -> String
+    
+    var selectedLocationUpdatedSubject: PublishSubject<Void> { get }
 }
 
 class LocationsManager: LocationsManagerType {
     private let localDatabase: LocalDatabase
+    private let preferences: Preferences
+    
+    let selectedLocationUpdatedSubject = PublishSubject<Void>()
 
-    init(localDatabase: LocalDatabase) {
+    init(localDatabase: LocalDatabase, preferences: Preferences) {
         self.localDatabase = localDatabase
+        self.preferences = preferences
     }
 
     func getBestLocationModel(from groupId: String) -> BestLocationModel? {
@@ -41,5 +54,27 @@ class LocationsManager: LocationsManagerType {
         guard let serverResultSafe = serverResult, let groupResultSafe = groupResult else { throw VPNConfigurationErrors.locationNotFound(groupId)
         }
         return (serverResultSafe, groupResultSafe)
+    }
+    
+    func saveLastSelectedLocation(with locationID: String) {
+        preferences.saveLastSelectedLocation(with: locationID)
+        selectedLocationUpdatedSubject.onNext(())
+    }
+
+    func saveBestLocation(with locationID: String) {
+        preferences.saveBestLocation(with: locationID)
+    }
+
+    func selectBestLocation(with locationID: String) {
+        saveLastSelectedLocation(with: locationID)
+        saveBestLocation(with: locationID)
+    }
+    
+    func getBestLocation() -> String {
+        preferences.getBestLocation()
+    }
+    
+    func getLastSelectedLocation() -> String {
+        preferences.getLastSelectedLocation()
     }
 }
