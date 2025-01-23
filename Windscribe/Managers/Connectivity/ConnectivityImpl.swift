9b3d31453e677fa44e4bb69a31ca8f918bf843d9
Ginder Singh <ginder@windscribe.com>
2025-01-23 14:40:13 -0500

    lint cleanup.

--- Windscribe/Managers/Connectivity/ConnectivityImpl.swift
+++ Windscribe/Managers/Connectivity/ConnectivityImpl.swift
@@ -13,10 +13,10 @@ class ConnectivityImpl: Connectivity {
     /// Observe this subject to get network change events.
     let network: BehaviorSubject<AppNetwork> = BehaviorSubject(value: AppNetwork(.disconnected))
     private let monitor = NWPathMonitor()
-    private var lastEvent: AppNetwork? // Cache the last emitted network event
-    private var debounceTimer: Timer? // Timer for event bouncing
+    private var lastEvent: AppNetwork?
+    private var debounceTimer: Timer?
     private var lastValidNetworkName: String?
-
+    
     init(logger: FileLogger) {
         self.logger = logger
         registerNetworkPathMonitor()
@@ -37,7 +37,7 @@ class ConnectivityImpl: Connectivity {
             self.refreshNetworkPathMonitor(path: self.monitor.currentPath)
         }
     }
-
+    
     /// Adds listener to network path monitor and builds network change events.
     private func registerNetworkPathMonitor() {
         monitor.pathUpdateHandler = { [weak self] path in
@@ -56,16 +56,16 @@ class ConnectivityImpl: Connectivity {
         let networkType = getNetworkType(path: path)
         getNetworkName(networkType: networkType) { [weak self] ssid in
             guard let self = self else { return }
-
+            
             var networkName = ssid
             if networkName == nil && networkType == .wifi {
                 networkName = self.lastValidNetworkName
             }
-
+            
             if networkName != nil && networkType == .wifi {
                 self.lastValidNetworkName = networkName
             }
-
+            
             let appNetwork = AppNetwork(
                 self.getNetworkStatus(path: path),
                 networkType: networkType,
@@ -80,7 +80,7 @@ class ConnectivityImpl: Connectivity {
             lastEvent = appNetwork
         }
     }
-
+    
     func awaitNetwork(maxTime: Double) async throws {
         let timeout: TimeInterval = maxTime
         let startTime = Date()
