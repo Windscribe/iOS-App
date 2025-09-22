f4621c7f8f21daec6b22f7a806027c4255f9e72e
Ginder Singh <ginder@windscribe.com>
2025-09-22 13:38:29 -0500

    Merge branch 'soner/fix/829-preferences-learn-links' into 'main'
    Fix #829: Problem Learn More Links in Prefrences
    
    See merge request ws/client/iosapp!1011
    
    (cherry picked from commit 45dce80d4a7905676e96c9b4d9affdc2271a9562)

--- Windscribe/Modules/Preferences/Components/Views/DescriptionWithLearnMore.swift
+++ Windscribe/Modules/Preferences/Components/Views/DescriptionWithLearnMore.swift
@@ -7,37 +7,21 @@
 //
 
 import SwiftUI
-import UIKit
 
 struct DescriptionWithLearnMore: View {
     let description: String
     var isDarkMode: Bool
     let action: () -> Void
+
     var body: some View {
         Text(buildAttributedString())
             .foregroundColor(.from(.infoColor, isDarkMode))
             .font(.regular(.footnote))
             .multilineTextAlignment(.leading)
-            .overlay(
-                GeometryReader { geo in
-                    Color.clear
-                        .contentShape(Rectangle()) // Enables tapping in full area
-                        .gesture(
-                            DragGesture(minimumDistance: 0)
-                                .onEnded { gesture in
-                                    if let range = fullText.range(of: TextsAsset.learnMore),
-                                       let boundingBox = boundingRectForSubstring(fullText: fullText,
-                                                                                  font: .regular(textStyle: .footnote),
-                                                                                  containerWidth: geo.size.width,
-                                                                                  range: NSRange(range, in: fullText)) {
-                                        if boundingBox.contains(CGPoint(x: gesture.location.x, y: gesture.location.y)) {
-                                            action()
-                                        }
-                                    }
-                                }
-                        )
-                }
-            )
+            .environment(\.openURL, OpenURLAction { _ in
+                action()
+                return .handled
+            })
     }
 
     private var fullText: String {
@@ -48,32 +32,8 @@ struct DescriptionWithLearnMore: View {
         var attributed = AttributedString(fullText)
         if let range = attributed.range(of: TextsAsset.learnMore) {
             attributed[range].foregroundColor = .learnBlue
+            attributed[range].link = URL(string: "learn-more://")
         }
         return attributed
-    }
-
-    func boundingRectForSubstring(fullText: String, font: UIFont, containerWidth: CGFloat, range: NSRange) -> CGRect? {
-        let attributedText = NSAttributedString(string: fullText, attributes: [.font: font])
-
-        let textStorage = NSTextStorage(attributedString: attributedText)
-        let layoutManager = NSLayoutManager()
-        let textContainer = NSTextContainer(size: CGSize(width: containerWidth, height: .greatestFiniteMagnitude))
-
-        textContainer.lineFragmentPadding = 0
-        textContainer.lineBreakMode = .byWordWrapping
-        textContainer.maximumNumberOfLines = 0
-
-        layoutManager.addTextContainer(textContainer)
-        textStorage.addLayoutManager(layoutManager)
-
-        // Ensure layout is computed
-        layoutManager.ensureLayout(for: textContainer)
-
-        // Get bounding rect for glyphs in the given range
-        var glyphRange = NSRange()
-        layoutManager.characterRange(forGlyphRange: range, actualGlyphRange: &glyphRange)
-        let boundingRect = layoutManager.boundingRect(forGlyphRange: glyphRange, in: textContainer)
-
-        return boundingRect
     }
 }
