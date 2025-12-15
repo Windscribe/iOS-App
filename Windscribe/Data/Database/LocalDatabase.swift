1534dd3e188fbebd99ebbcda1a61fed2155df1be
Andre Fonseca <andre@windscribe.com>
2025-12-15 11:37:26 +0000

    also the DB does not need to be async anymore

--- Windscribe/Data/Database/LocalDatabase.swift
+++ Windscribe/Data/Database/LocalDatabase.swift
@@ -50,7 +50,7 @@ protocol LocalDatabase {
     func removeNetwork(wifiNetwork: WifiNetwork)
     func addPingData(pingData: PingData)
     func getAllPingData() -> [PingData]
-    func saveCustomConfig(customConfig: CustomConfig) async
+    func saveCustomConfig(customConfig: CustomConfig)
     func removeCustomConfig(fileId: String)
     func getCustomConfig() -> Observable<[CustomConfig]>
     func getPorts(protocolType: String) -> [String]?
